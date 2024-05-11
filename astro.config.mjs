3d061d6e41197bf575d43e9be2e5a6a5d21ed23d
Santiago1809 <aristizabalsantiago482@gmail.com>
2024-05-11 00:51:45 -0500

    Correciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
     css: {
-      devSourcemap: true, // Genera sourcemaps para CSS en modo desarrollo
+      devSourcemap: true // Genera sourcemaps para CSS en modo desarrollo
     }
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
